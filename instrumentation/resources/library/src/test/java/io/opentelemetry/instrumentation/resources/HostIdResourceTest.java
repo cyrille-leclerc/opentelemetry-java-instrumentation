3a9fb8027f43d4babd16aaddf4b7df414e537893
Cyrille Le Clerc <cyrille.leclerc@grafana.com>
2025-03-13 23:00:22 +0100

    Implement HostIdResourceProvider for Mac OS X

--- instrumentation/resources/library/src/test/java/io/opentelemetry/instrumentation/resources/HostIdResourceTest.java
+++ instrumentation/resources/library/src/test/java/io/opentelemetry/instrumentation/resources/HostIdResourceTest.java
@@ -11,6 +11,8 @@ import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.sdk.autoconfigure.spi.internal.DefaultConfigProperties;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.semconv.incubating.HostIncubatingAttributes;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collection;
@@ -53,6 +55,19 @@ class HostIdResourceTest {
     }
   }
 
+  private static class MacOsxTestCase {
+    private final String name;
+    private final String expectedValue;
+    private final Supplier<InputStream> queryMacOsxSpHardwareDataType;
+
+    public MacOsxTestCase(
+        String name, String expectedValue, Supplier<InputStream> queryMacOsxSpHardwareDataType) {
+      this.name = name;
+      this.expectedValue = expectedValue;
+      this.queryMacOsxSpHardwareDataType = queryMacOsxSpHardwareDataType;
+    }
+  }
+
   @TestFactory
   Collection<DynamicTest> createResourceLinux() {
     return Stream.of(
@@ -64,7 +79,7 @@ class HostIdResourceTest {
                     testCase.name,
                     () -> {
                       HostIdResource hostIdResource =
-                          new HostIdResource(() -> "linux", testCase.pathReader, null);
+                          new HostIdResource(() -> "linux", testCase.pathReader, null, null);
 
                       assertHostId(testCase.expectedValue, hostIdResource);
                     }))
@@ -89,7 +104,47 @@ class HostIdResourceTest {
                     () -> {
                       HostIdResource hostIdResource =
                           new HostIdResource(
-                              () -> "Windows 95", null, testCase.queryWindowsRegistry);
+                              () -> "Windows 95", null, testCase.queryWindowsRegistry, null);
+
+                      assertHostId(testCase.expectedValue, hostIdResource);
+                    }))
+        .collect(Collectors.toList());
+  }
+
+  @TestFactory
+  Collection<DynamicTest> createResourceMacOsx() {
+    String input =
+        """
+        {
+          "SPHardwareDataType" : [
+            {
+              "_name" : "hardware_overview",
+              "activation_lock_status" : "activation_lock_enabled",
+              "boot_rom_version" : "11881.81.4",
+              "chip_type" : "Apple M2",
+              "machine_model" : "Mac14,2",
+              "machine_name" : "MacBook Air",
+              "model_number" : "Z15S0030VFN/A",
+              "number_processors" : "proc 8:4:4",
+              "os_loader_version" : "11881.81.4",
+              "physical_memory" : "24 GB",
+              "platform_UUID" : "12345678-1234-1234-1234-1234567890AB",
+              "provisioning_UDID" : "12345678-1234567890ABCDE1",
+              "serial_number" : "1234567890"
+            }
+          ]
+        }""";
+    return Stream.of(
+            new MacOsxTestCase("default", "test", () -> new ByteArrayInputStream(input.getBytes())),
+            new MacOsxTestCase("empty file or error reading", null, () -> null))
+        .map(
+            testCase ->
+                DynamicTest.dynamicTest(
+                    testCase.name,
+                    () -> {
+                      HostIdResource hostIdResource =
+                          new HostIdResource(
+                              () -> "Mac OS X", null, null, testCase.queryMacOsxSpHardwareDataType);
 
                       assertHostId(testCase.expectedValue, hostIdResource);
                     }))
